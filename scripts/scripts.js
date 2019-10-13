110d6d3005dba88b2302e3658ab01e90ae1ec6de
Shawn Walcher <swalcher@ClutZ-Mac.local>
2019-10-13 13:47:59 -0500

    first commit

---
+++ scripts/scripts.js
@@ -0,0 +1,21 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+  var index;
+  var slideArray = document.querySelectorAll(".mySlides");
+  if (n > slideArray.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slideArray.length;
+  }
+  for (index = 0; index < slideArray.length; index++) {
+    slideArray[index].style.display = "none";
+  }
+  slideArray[slideIndex - 1].style.display = "block";
+}
